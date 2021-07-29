64168094235151e34fab66f0e2a15198f09cd7e3
DavidMoraes-DEV <71403807+DavidMoraes-DEV@users.noreply.github.com>
2021-07-29 14:30:03 -0300

    Adicionar arquivos de projeto.

---
+++ BusinessAccount/Entities/BusinessAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace Accounts.Entities
+{
+    class BusinessAccount : Account //Aplicando a herança de uma superclasse (Account) para a Subclasse (BusinessAccount) nesse exemplo
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount () {}
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance) //Utilizando os atributos e construtores da Super classe que foi extendida para essa Sub classe
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan (double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ BusinessAccount/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Accounts.Entities;
+
+namespace Accounts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brow", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200; -> Nessa caso não será possível alterar o valor do Balance pois o SET do Balance esta protegido e apenas a super classe ou subclasse poderá modificar seu valor.
+
+        }
+    }
+}
